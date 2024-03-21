0a0c59fe3b60c0ba6f0780bf2f5a493ab26192e3
cordelia <snapecordelia@gmail.com>
2024-03-21 14:23:08 +0000

    better error handling but brokem

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,11 +1,13 @@
 "use client";
 
-export default function Error() {
+export default function Error({ error, reset }) {
+  console.log(error);
   return (
     <div className="w-screen h-screen flex justify-center items-center bg-red-300 text-white">
       <div className="text-center">
         <h2 className="text-2xl font-bold mb-4">Error</h2>
         <p>Something went wrong...</p>
+        <button onClick={() => reset()}>Try again</button>
       </div>
     </div>
   );
